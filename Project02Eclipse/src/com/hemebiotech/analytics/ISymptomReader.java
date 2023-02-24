040718ea14fa50909971a3fb4335d6aba27d9711
hilde <hildejacobi@yahoo.com>
2023-02-24 10:28:03 -0600

    commit friday 24 february 2023

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,25 @@
+
 package com.hemebiotech.analytics;
 
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+
+
 
 /**
- * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
  * 
- * The implementation does not need to order the list
+ * This is the Interface ISymptomReader 
  * 
+ * @author hilde Jacobi
+ * @version 1 Build February 23, 2023.
+ *
  */
+
 public interface ISymptomReader {
-	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
-	 */
-	List<String> GetSymptoms ();
+	
+	public List<String> readFile(String filePath);
+
+	public void printFile(Map<String,Integer> sortedSymptoms ,String outputPath) throws IOException;
 }
